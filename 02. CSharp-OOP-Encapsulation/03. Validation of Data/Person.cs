5acda54a59aea2f99448bad88370084be654f2bf
ahmedsaliev <ahmed.saliev@gmail.com>
2019-07-28 10:37:44 +0300

    Added new files

---
+++ 02. CSharp-OOP-Encapsulation/03. Validation of Data/Person.cs
@@ -0,0 +1,102 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private decimal salary;
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(
+            string firstName,
+            string lastName,
+            int age,
+            decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                this.salary = value;
+            }
+        }
+
+        private void ValidateName(string name)
+        {
+            if (name.Length < 3)
+            {
+                throw new ArgumentException("Name cannot contain fewer than 3 symbols!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ 02. CSharp-OOP-Encapsulation/04. First and Reserve Team/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int playersCount = int.Parse(Console.ReadLine());
+
+            Team players = new Team("SoftUni");
+
+            for (int i = 0; i < playersCount; i++)
+            {
+                var input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+                Person player = new Person(firstName, lastName, age, salary);
+
+                players.AddPlayer(player);
+            }
+
+            players.GetPlayersCount();
+        }
+    }
+}
