92e8db44186e4b741f32ff6eedbf8d06fc69e508
ahmedsaliev <ahmed.saliev@gmail.com>
2019-08-18 17:41:23 +0300

    new content

---
+++ 02. CSharp-OOP-Encapsulation/Problem 1. Class Box Data/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        double length = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        try
+        {
+            Box box = new Box(length, width, height);
+            Console.WriteLine("Surface Area - {0:F2}", box.GetSurfaceArea());
+            Console.WriteLine("Lateral Surface Area - {0:F2}", box.GetLateralSurfaceArea());
+            Console.WriteLine("Volume - {0:F2}", box.GetVolume());
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+
+        
+    }
+}
